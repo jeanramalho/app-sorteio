f9f579292db2ace82925485cd20893aac7133cf0
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-19 14:13:07 -0300

    feat(app): Finaliza aplicativo de sorteio

--- app-sorteio/ViewController.swift
+++ app-sorteio/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        var numero =  arc4random_uniform(100)
+        
+        legendaResultado.text = String(numero)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
